bc87f2fc0b1cb539f4701c7b4fb2161873944a2a
petergich <petergich80@gmail.com>
2023-11-07 19:35:58 +0300

    message

---
+++ hotel/restorant/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+ 
+admin.site.register(food_category)
+admin.site.register(food)
+# Register your models here.
---
+++ hotel/restorant/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RestorantConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'restorant'
