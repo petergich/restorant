29fff57e2c1ed3f31743ca6c8854aa07168abc90
petergich <petergich80@gmail.com>
2023-11-09 03:22:38 +0300

    peter

--- hotel/restorant/models.py
+++ hotel/restorant/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class food_category(models.Model):
     name=models.CharField(max_length=254,unique=True)
-    def str(self):
+    def __str__ (self):
         return self.name
 class food(models.Model):
     name=models.CharField(max_length=254)
     category=models.ForeignKey(food_category,on_delete=models.CASCADE)
     price=models.IntegerField()
-    def str(self):
+    def ___str__ (self):
         return self.name
 # Create your models here.
--- hotel/restorant/views.py
+++ hotel/restorant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import *
+
 
 def home(request):
-    return render(request,"index.html",)
+    categories=food_category.objects.all()
+    foods=food.objects.all()
+    objects=[]
+    for cat in categories:
+        cat_foods=[]
+        for foo in foods:
+            if foo.category == cat:
+                cat_foods.append(foo)
+        if cat_foods!=[]:
+            objects.append({"category":cat,"foods":cat_foods})
+    context={"objects":objects}
+    return render(request,"index.html",context)
 # Create your views here.
